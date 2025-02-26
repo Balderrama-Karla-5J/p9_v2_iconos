da859105a6fef02101f496e677e18b046cf9ea1e
KARLA_ALEJANDRA_BALDERRAMA_PADILLA <a.22308051281149@cbtis128.edu.mx>
2025-02-26 20:34:30 +0000

    cambios

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,66 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisIconos());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Karla Balderrama Mat:22308051281149',
+              style: TextStyle(fontSize: 18)),
+          backgroundColor: Color(0xff93c7f2),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment:
+                MainAxisAlignment.center, // Centra las filas verticalmente
+            children: <Widget>[
+              // Primera fila
+              Row(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Centra los iconos en la fila
+                children: <Widget>[
+                  _buildIconWithSubtitle(Icons.star, 'Favorito', Colors.yellow),
+                  SizedBox(width: 20), // Espacio entre los iconos
+                  _buildIconWithSubtitle(
+                      Icons.favorite, 'Me gusta', Colors.red),
+                  SizedBox(width: 20), // Espacio entre los iconos
+                  _buildIconWithSubtitle(
+                      Icons.book, 'Libro', Color(0xff36e7f4)),
+                ],
+              ),
+              SizedBox(height: 20), // Espacio entre las filas
+              // Segunda fila
+              Row(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Centra los iconos en la fila
+                children: <Widget>[
+                  _buildIconWithSubtitle(Icons.thumb_up, 'Like', Colors.blue),
+                  SizedBox(width: 20), // Espacio entre los iconos
+                  _buildIconWithSubtitle(
+                      Icons.account_box_sharp, 'Perfil', Color(0xff020101)),
+                  SizedBox(width: 20), // Espacio entre los iconos
+                  _buildIconWithSubtitle(Icons.menu, 'Menu', Colors.black),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  // Método para construir un ícono con subtítulo y color personalizado
+  Widget _buildIconWithSubtitle(IconData icon, String subtitle, Color color) {
+    return Column(
+      children: <Widget>[
+        Icon(icon, size: 50.0, color: color), // Aplica el color al ícono
+        SizedBox(height: 8), // Espacio entre el ícono y el subtítulo
+        Text(subtitle, style: TextStyle(fontSize: 16)),
+      ],
     );
   }
 }
